df61476cbb4e3fd44e299fec6da7f754aa7698be
Jaeyoon Song <jyo3on@gmail.com>
2021-01-14 23:37:52 +0900

    add js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,50 @@
 //= require libs/bootstrap.min
 //= require turbolinks
 //= require_tree .
+
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+  
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Fallback: Copying text command was ' + msg);
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err);
+  }
+
+  document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(function() {
+    alert("링크 복사 성공!");
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err);
+    alert("죄송합니다. 에러가 발생했습니다. 다시 시도해주세요.")
+  });
+}
+function CopyLink() {
+  copyTextToClipboard(location.href);
+}
+
+function openInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
+function ShareFacebook() {
+  openInNewTab("http://www.facebook.com/sharer.php?u=" + location.href)
+}

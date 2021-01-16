8582df33a2893ad9c0014944df483674d569f7f9
Jaeyoon Song <jyo3on@gmail.com>
2021-01-17 05:46:35 +0900

    admin dashboards

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :events
+    resources :user_ones
+    resources :user_twos
+
+    root to: "events#index"
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "events#index"
   get '/events/:id', to: 'events#show'
   get '/events/:id/apply', to: 'events#apply'
 
-  post '/users/:id/new', to: 'events#create_user_one'
-  post '/users/:id/verify', to: 'events#verify_user_one'
+  post '/users/1/new', to: 'events#create_user_one'
+  post '/users/1/verify', to: 'events#verify_user_one'
+  post '/users/1/confirm', to: 'events#confirm_user_one'
 
 
   get '/finished', to: 'events#finished'
